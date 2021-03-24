a25eed70ea407f2d4886d744724f0fb09580d63a
Heba <heba.mohamed16@stjohns.edu>
2021-03-24 02:12:28 +0200

    Assignment_6
    Answers to assignment 6

---
+++ my_packages/operations/math.py
@@ -0,0 +1,5 @@
+def get_average(l):
+    l_sum = sum(l)
+    l_count = len(l)
+    average_l = l_sum /l_count
+    return average_l
---
+++ my_packages/transformers/StringTransformer.py
@@ -0,0 +1,8 @@
+def reverse(word):
+   r_word = word[::-1]
+   return r_word
+ 
+   
+def capital(x):
+    x_capital = x.capitalize()
+    return x_capital
